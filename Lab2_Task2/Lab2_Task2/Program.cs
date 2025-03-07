13ecba52beebd02633941a28f8dbbc07bdeb5976
noor123mia <miasultan369@gmail.com>
2025-03-07 12:46:17 +0500

    Add files via upload

---
+++ Lab2_Task2/Lab2_Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter input:");
+        string input = Console.ReadLine();
+
+        // Split the input based on spaces
+        string[] words = input.Split(' ');
+
+        // Regular Expression for relational operators
+        Regex regex1 = new Regex("^(==|!=|<=|>=|<|>)$");
+
+        Console.WriteLine("Relational operators found:");
+        foreach (string word in words)
+        {
+            if (regex1.IsMatch(word))
+            {
+                Console.WriteLine($"Valid: {word}");
+            }
+            else
+            {
+                Console.WriteLine($"Invalid: {word}");
+            }
+        }
+        Console.ReadKey();
+    }
+}
