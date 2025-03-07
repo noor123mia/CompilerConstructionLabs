58d620dae432f8091d46efb0388b72ee70186505
noor123mia <miasultan369@gmail.com>
2025-03-07 12:44:55 +0500

    Add files via upload

---
+++ Lab2_Task1/Lab2_Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter input:");
+        string input = Console.ReadLine();
+
+        // Split the input based on spaces
+        string[] words = input.Split(' ');
+
+        // Regular Expression for logical operators (AND, OR, NOT, XOR)
+
+        Regex regex = new Regex(@"^(and|or|not|\|\||&&|!|\^|&|\||~|<<|>>|\?\?|:\?)$");
+
+
+        Console.WriteLine("Logical operators found:");
+        foreach (string word in words)
+        {
+            if (regex.IsMatch(word))
+            {
+                Console.WriteLine($"Valid: {word}");
+            }
+            else
+            {
+                Console.WriteLine($"Invalid: {word}");
+            }
+        }
+        Console.ReadKey();
+    }
+}
